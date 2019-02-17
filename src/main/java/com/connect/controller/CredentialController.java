f4fed47aefa98f776ac2994878ea90b61296e80d
Swapnil Gaikwad <swapnil.gaikwad@pubmatic.com>
2019-02-17 17:28:23 +0530

    Add logger to spring project

--- src/main/java/com/connect/controller/CredentialController.java
+++ src/main/java/com/connect/controller/CredentialController.java
@@ -3,6 +3,8 @@ package com.connect.controller;
 import com.connect.model.Credential;
 import com.connect.model.Link;
 import com.connect.service.CredentialService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/credential")
 class CredentialController {
     private CredentialService service;
+    private static Logger logger = LoggerFactory.getLogger(Credential.class);
 
     @Autowired
     CredentialController(CredentialService service) {
@@ -19,12 +22,14 @@ class CredentialController {
     @PostMapping("/create")
     @ResponseBody
     Link createLink(@RequestBody Credential credential) {
+        logger.info("In createLink");
         return service.add(credential);
     }
 
     @GetMapping("/get")
     @ResponseBody
     Credential getCredential(@RequestParam("identifier") String identifier) {
+        logger.info("In getCredential");
         return service.get(identifier);
     }
 
